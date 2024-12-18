e00ef9270805c0b833022192ee750b70a4beeed3
Erick Rodriguez <e2350347239@live.uleam.edu.ec>
2024-12-17 23:07:22 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,9 @@ php artisan view:cache
 # Listar rutas para verificar que todo está en orden
 php artisan route:list
 chmod -R 775 /var/www/html/database
+# Ajustar permisos del archivo
+chmod 644 /var/www/html/api/sad.svg
+
+# Ajustar permisos del directorio
+chmod 755 /var/www/html/api
 php artisan serve --host=0.0.0.0 --port=10000
