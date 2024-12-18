47ea22d8573009d2c32961754914b7316b8e8c1c
Erick Rodriguez <e2350347239@live.uleam.edu.ec>
2024-12-17 23:24:30 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # Asegúrate de que se detengan en caso de error
 set -e
@@ -5,23 +6,22 @@ set -e
 echo "Instalando Faker..."
 composer require fakerphp/faker --dev
 # Instalar dependencias de Composer
+echo "Instalando dependencias de Composer..."
 composer install --no-dev --optimize-autoloader
 # Ejecutar migraciones
+echo "Ejecutando migraciones..."
 php artisan migrate --force
+# Ejecutar seeding si es necesario
+echo "Ejecutando seeders..."
+php artisan db:seed --class=RolesAndPermissionsSeeder
 # Cachear configuraciones
 echo "Cacheando configuraciones..."
 php artisan config:cache
-# Optimizacion
+# Cachear rutas
+echo "Cacheando rutas..."
 php artisan route:cache
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 # Listar rutas para verificar que todo está en orden
+echo "Listando rutas..."
 php artisan route:list
 chmod -R 775 /var/www/html/database
-# Ajustar permisos del archivo
-chmod 644 /var/www/html/api/sad.svg
-
-# Ajustar permisos del directorio
-chmod 755 /var/www/html/api
 php artisan serve --host=0.0.0.0 --port=10000
