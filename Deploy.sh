4ce6b4711878755895a78d2a8f656837e1548dc7
Erick Rodriguez <e2350347239@live.uleam.edu.ec>
2024-12-17 22:35:11 -0500

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -6,7 +6,6 @@ composer install --no-dev --optimize-autoloader
 # Generar clave de aplicación
 php artisan key:generate
 # Ejecutar migraciones
-echo "Ejecutando migraciones..."
 php artisan migrate --force
 # Optimizacion
 php artisan route:cache
@@ -14,7 +13,6 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 # Listar rutas para verificar que todo está en orden
-echo "Listando rutas..."
 php artisan route:list
 chmod -R 775 /var/www/html/database
 php artisan serve --host=0.0.0.0 --port=10000
